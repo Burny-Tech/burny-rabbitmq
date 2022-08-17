ae1db475e478c9d673daf7dc85a98bb0dd60d3bc
chenyingxin <1402421202@qq.com>
2022-08-17 23:43:51 +0800

    [update]P13-初始化

---
+++ burny-rabbitmq-01/src/main/java/com/burny/rabbitmq/RabbitMqApplication.java
@@ -0,0 +1,18 @@
+package com.burny.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Note TODO
+ * @Author cyx
+ * @Date 2022/8/17 23:28
+ */
+
+
+@SpringBootApplication
+public class RabbitMqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqApplication.class, args);
+    }
+}
